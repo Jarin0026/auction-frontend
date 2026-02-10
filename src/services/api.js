2966a78b602f5363f3b5e2c16d4804dd9795c2e3
Niraj Kumar Mahto <nirajmahto26@gmail.com>
2026-02-11 01:55:59 +0530

    Update API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:import.meta.env.VITE_API_URL,
+  baseURL:https://auction-backend-sx91.onrender.com,
 });
 
 api.interceptors.request.use((config) => {
