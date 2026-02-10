14c4099acb0d48606389c07b1d3d2eeae8effecd
Niraj Kumar Mahto <nirajmahto26@gmail.com>
2026-02-11 01:58:08 +0530

    Fix baseURL formatting in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:https://auction-backend-sx91.onrender.com,
+  baseURL: "https://auction-backend-sx91.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  console.log("TOKEN SENT:", token);
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
